100ae1173d94809308eaa3006a705e058b7d8c64
igor <gomesoliveirasl02@hotmail.com>
2022-07-12 18:57:51 -0300

    setinterval

---
+++ ConteudoEstudado/funçõesnativasjs/setinterval.js
@@ -0,0 +1,22 @@
+function mostrarHora(){
+    let data = new Date();
+
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false
+    })
+}
+
+// function funcaoDoInterval(){
+//     console.log(mostrarHora())
+// }
+
+// setInterval(funcaoDoInterval, 1000);
+
+//ou
+
+setInterval(function(){
+    console.log(mostrarHora())
+}, 1000)
+
+
+
